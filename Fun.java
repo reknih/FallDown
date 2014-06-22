5480db8550b20c5efbe9e7e606df1fd321892edb
Marcel Ohlig <marcelohlig@googlemail.com>
2014-06-22 17:53:04 +0200

    Gameover & Wände verbessert

--- Fun.java
+++ Fun.java
@@ -14,14 +14,14 @@ public class Fun extends World
      * 
      */
     private int step;
-    public final int MAXSTEP = 20;
+    public final int MAXSTEP = 96;
     
     public final int WIDTH   = 1200;
     public final int HEIGHT  = 800;
 
     public Fun()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+       
         super(1200, 800, 1); 
         Greenfoot.setSpeed(10);
         step = MAXSTEP;
@@ -32,13 +32,14 @@ public class Fun extends World
         if (step != 0)
             return;
         step = MAXSTEP;
-        for (int i = WIDTH; i >= 0; i = i - 100) {
-            if(Greenfoot.getRandomNumber(36) < 23)
+        for (int i = WIDTH; i >= 0; i = i - 66) {
+            if(Greenfoot.getRandomNumber(50) < 26)
             {
                 addObject(new Wand(), i, 0);
             }
         }
     }
+    
 }
 
 
